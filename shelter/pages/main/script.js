a2d2bb8b882d75f45879b383f4fd07800a014a12
unknown <rustamov.fozil.2011@gmail.com>
2022-10-29 14:29:33 +0500

    remote AOS

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,14 +1,13 @@
 const btn = document.querySelector('.obr__pets-btn');
-const btn1 = document.querySelector('.obr__pets-btn-1');
-const btn2 = document.querySelector('.obr__pets-btn-2');
 const modalContainer = document.querySelector('.modal-container');
-const modalContainer1 = document.querySelector('.modal-container1');
-const modalContainer2 = document.querySelector('.modal-container2');
 
 btn.addEventListener('click', function(){
     modalContainer.classList.add('active')
 })
 
-btn1.addEventListener('click', function(){
-    modalContainer1.classList.add('active-1')
+// const burgerMenu = document.querySelector('.nav__burger')
+// const headerList = document.querySelector('.hn__list')
+
+burgerMenu.addEventListener('click', ()=> {
+    headerList.classList.add('active')
 })
